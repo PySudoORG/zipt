8e8e883ccb2216fe5db19e0f272c67d37aa0d0ec
PySudoORG <139513562+PySudoORG@users.noreply.github.com>
2023-12-11 23:24:30 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
-<?php
-$dirPath = '/home';
-$zipFileName = 'ok.zip';
-
-$zip = new ZipArchive();
-if ($zip->open($zipFileName, ZipArchive::CREATE) === TRUE){
-    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dirPath));
-    foreach ($files as $file){
-        $filePath = $file->getRealPath();
-        $relativePath = substr($filePath, strlen($dirPath) + 1);
-        if (!is_dir($filePath)){
-            $zip->addFile($filePath, $relativePath);
-        }
+function zipDirectory($source, $destination)
+{
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
     }
-    $zip->close();
-    echo 'دایرکتوری فشرده‌سازی و در فایل ' . $zipFileName . ' ذخیره شد.';
-} else {
-    echo 'خطا در ایجاد فایل زیپ';
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+
+    $source = realpath($source);
+
+    if (is_dir($source)) {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file) {
+            $file = realpath($file);
+
+            if (is_dir($file)) {
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            } elseif (is_file($file)) {
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    } elseif (is_file($source)) {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+
+    return $zip->close();
 }
-?>
+
+// استفاده:
+$source = '/home';
+$destination = './tt.zip';
+
+if (zipDirectory($source, $destination)) {
+    echo 'فایل ZIP با موفقیت ایجاد شد!';
+} else {
+    echo 'مشکلی در ایجاد فایل ZIP به وجود آمد!';
+}
